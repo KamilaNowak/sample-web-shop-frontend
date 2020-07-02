fbd1ca03fbcbd8544541e40c755ab1efc2ecac3c
KamilaNowak <knowak242@gmail.com>
2020-07-02 09:58:54 +0200

    add angular starter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsListComponent } from './components/items-list/items-list.component';
+import {HttpClientModule }from '@angular/common/http'
+import { ItemService } from '../app/services/item.service'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/utils/item.ts
@@ -0,0 +1,12 @@
+export class Item {
+
+    serialNumber: string;
+    isActive: boolean;
+    title: string;
+    description: string;
+    price: number;
+    availability: number;
+    imageUrl: string;
+    createdAt:Date;
+    lastUpdatedAt: Date;
+}
