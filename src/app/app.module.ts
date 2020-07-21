ccabd64c49e696484e30559445597e6db1068c55
KamilaNowak <knowak242@gmail.com>
2020-07-21 16:31:02 +0200

    add component udpate and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,16 @@ import { ItemsListComponent } from './components/items-list/items-list.component
 import { HttpClientModule } from '@angular/common/http' //for HTTP Client
 import { ItemService } from '../app/services/item.service'
 import { MatTableModule } from '@angular/material/table'  
+import { Routes, RouterModule } from '@angular/router'
+
+const routes : Routes= [
+  {path: 'category/:id', component: ItemsListComponent},
+  {path: 'category', component: ItemsListComponent},
+  {path: 'items', component: ItemsListComponent},
+  {path: '', redirectTo:'/items', pathMatch:'full'},
+  {path: '**', redirectTo:'/items', pathMatch:'full'},
+
+]
 
 @NgModule({
   declarations: [
@@ -15,6 +25,7 @@ import { MatTableModule } from '@angular/material/table'
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     HttpClientModule,
     MatTableModule
